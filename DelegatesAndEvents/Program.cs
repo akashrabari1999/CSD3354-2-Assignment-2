7ed004d157e44fa10c944edcb119246006075699
visualstudio <visualstudio@123-PC42>
2019-03-09 13:32:22 -0500

    lab6

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System;
+
 namespace DelegatesAndEvents
 {
     public class Program
@@ -12,22 +14,36 @@ namespace DelegatesAndEvents
         {
             DelegateExercises delegateExercises = new DelegateExercises();
             delegateExercises.Method3();
+            Console.ReadLine();
         }
     }
 
     public class DelegateExercises
     {
-        public delegate int MyDelegate();
+        public delegate int MyDelegate(int intValue);
 
-        void Method1()
+        int Method1(int intMethod1)
         {
-            System.Console.WriteLine("MyDelegate");
+            return intMethod1 * 2;
+        }
+
+        int Method2(int intMethod1)
+        {
+            return intMethod1 * 10;
+        }
+
+        public void Method4(MyDelegate myDelegate)
+        {
+            int result = myDelegate(10);
+            Console.WriteLine(result);
         }
 
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
+            Method4(myDelegate);
+            myDelegate = new MyDelegate(Method2);
+            Method4(myDelegate);
         }
     }
 }
