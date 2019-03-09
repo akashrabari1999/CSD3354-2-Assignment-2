a7a336023a96f056c23cd2529e76055d38e050ec
visualstudio <visualstudio@123-PC42>
2019-03-09 13:18:16 -0500

    lab3

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -20,7 +20,7 @@ namespace DelegatesAndEvents
     public class DelegateExercises
     {
         public delegate void MyDelegate();
-        void Method1()
+        void Method1(int i)
         {
             Console.WriteLine("Method1");
             Console.ReadLine();
@@ -28,7 +28,7 @@ namespace DelegatesAndEvents
         public void Method2()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate(50);
+            myDelegate();
         }
 
     }
