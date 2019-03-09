83033fa00b9cfcb561179394bf92cd9da87c8650
visualstudio <visualstudio@123-PC42>
2019-03-09 13:14:54 -0500

    lab2

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -10,6 +10,26 @@ namespace DelegatesAndEvents
     {
         static void Main(string[] args)
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method2();
         }
+    }
+}
+namespace DelegatesAndEvents
+{
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate(50);
+        }
+
     }
 }
