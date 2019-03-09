7fc4808c6e6a1778aa415c608c2ce2e43e10801e
visualstudio <visualstudio@123-PC42>
2019-03-09 13:38:02 -0500

    lab11

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -8,36 +8,12 @@ using System;
 
 
 
-namespace DelegatesAndEvents
-{
-  public class Program
-{
-    public static void Main()
-    {
-        DelegateExercises delegateExercises = new DelegateExercises();
-        delegateExercises.Method3();
-        Console.ReadLine();
-    }
-}
-
 public delegate void MyDelegate();
 
 public class DelegateExercises
 {
-    void Method1()
+    void Method3()
     {
-        System.Console.WriteLine("Method1");
-
-
+        System.Console.WriteLine(MyDelegate.ToString());
     }
-
-    public void Method3()
-    {
-        MyDelegate myDelegate = new MyDelegate(Method1);
-        myDelegate();
-        System.Console.WriteLine(myDelegate.ToString());
-    }
-}
- 
- 
 }
